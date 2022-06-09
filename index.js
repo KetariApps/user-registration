55c3f40ff4087f07fdaafa37ed4f53a5c7b6b778
zachagarrett <zach.garrett.archery@gmail.com>
2022-06-09 11:57:45 -0500

    cors policy

--- index.js
+++ index.js
@@ -9,33 +9,56 @@ const neoUser = process.env.NEOUSER;
 const production = process.env.PRODUCTION;
 
 const typeDefs = gql`
-  type Person @node(label: "Person", additionalLabels: ["EmailList"]) {
+  type Application {
+    createdAt: DateTime! @timestamp(operations: [CREATE])
+    id: ID! @id
+    inSets: [Set!]! @relationship(type: "CONTAINS", direction: IN)
+    label: String!
+    people: [Person!]! @relationship(type: "USES", direction: IN)
+    hasSets: [Set!]! @relationship(type: "HAS", direction: OUT)
+    updatedAt: DateTime! @timestamp(operations: [UPDATE])
+  }
+  union Entity = Application | Idea | Identity | Person | Set
+  type Idea {
+    createdAt: DateTime! @timestamp(operations: [CREATE])
+    description: String!
+    id: ID! @id
+    inSets: [Set!]! @relationship(type: "CONTAINS", direction: IN)
+    person: [Person!]! @relationship(type: "HAS", direction: IN)
+    updatedAt: DateTime! @timestamp(operations: [UPDATE])
+  }
+  type Identity {
+    createdAt: DateTime! @timestamp(operations: [CREATE])
+    corroboratedIdentities: [Identity!]!
+      @relationship(type: "CORROBORATES", direction: OUT)
+    inSets: [Set!]! @relationship(type: "CONTAINS", direction: IN)
+    label: String!
+    person: [Person!]! @relationship(type: "HAS", direction: IN)
+    type: String!
+    # email, password, etc
+    value: String!
+    # secret | public
+  }
+  type Person {
     applications: [Application!]! @relationship(type: "USES", direction: OUT)
     createdAt: DateTime! @timestamp(operations: [CREATE])
-    email: String!
     firstName: String!
+    ideas: [Idea!]! @relationship(type: "HAS", direction: OUT)
+    identities: [Identity!]! @relationship(type: "USES", direction: OUT)
+    inSets: [Set!]! @relationship(type: "CONTAINS", direction: IN)
     lastName: String!
     updatedAt: DateTime! @timestamp(operations: [UPDATE])
   }
-  type Application {
+  type Set {
+    application: [Application!]! @relationship(type: "HAS", direction: IN)
     createdAt: DateTime! @timestamp(operations: [CREATE])
-    name: String! @unique
-    people: [Person!]! @relationship(type: "USES", direction: IN)
+    id: ID! @id
+    inSets: [Set!]! @relationship(type: "CONTAINS", direction: IN)
+    label: String!
+    entities: [Entity!]! @relationship(type: "CONTAINS", direction: OUT)
     updatedAt: DateTime! @timestamp(operations: [UPDATE])
   }
 `;
-class BasicLogging {
-  requestDidStart({ queryString, parsedQuery, variables }) {
-    const query = queryString || print(parsedQuery);
-    console.log(query);
-    console.log(variables);
-  }
-
-  willSendResponse({ graphqlResponse }) {
-    console.log(JSON.stringify(graphqlResponse, null, 2));
-  }
-}
-
 const driver = neo4j.driver(neoUri, neo4j.auth.basic(neoUser, neoPass));
 if (production !== "true") console.log(process.env, driver);
 
@@ -45,7 +68,10 @@ neoSchema.getSchema().then((schema) => {
   const server = new ApolloServer({
     schema: schema,
     introspection: production !== "true",
-    extensions: production !== "true" && [() => new BasicLogging()],
+    csrfPrevention: true,
+    cors: {
+      origin: ["https://www.ecubed.ai", "https://www.ketari.io"],
+    },
   });
 
   server.listen(port || 5000).then(({ url }) => {
